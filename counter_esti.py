0210dce4793546efae74dc3932d3e1c81b96014f
ogr0818 <sammy336588@gmail.com>
2025-12-03 13:15:27 +0800

    set up the parameters

--- counter_esti.py
+++ counter_esti.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 
-st.title("稱重轉顆數")
+st.title("藥包機顆數估算")
 id = st.text_input("請輸入藥盒編號： ", value='')
 
 df = pd.read_excel("machine_meta.xlsx")
@@ -19,10 +19,19 @@ else:
     st.divider()
     regression = pd.read_excel("deming.xlsx")
     para = regression.query('編號 == @num')
+
     wt = st.text_input("重量： ", value=0)
     tab_real = st.text_input("實際顆數： ", value='')
     b0 = float(para.values.tolist()[0][1])
     b1 = float(para.values.tolist()[0][2])
     tab = np.round((float(wt) - b0)/ b1)
     st.write(f'估計顆數： {int(tab)}')
-    st.write(para)
+    st.divider()
+if st.button("確定記錄本筆資料", type='primary'):
+    st.write(f"{x[0][2]} 存入一筆資料")
+    st.write(f"總重：{wt} 共{tab_real}顆")
+
+# with st.sidebar:
+#     st.markdown('<h2 style="color:blue;font-size:24px">存檔輸出(.xlsx)： </h2>', unsafe_allow_html=True)
+#     if st.button("Saved", type="primary"):
+#         st.write("finished")
