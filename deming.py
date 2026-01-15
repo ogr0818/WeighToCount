12cc8564ae2d4248108293be1e455cfe9bd97feb
ogr0818 <sammy336588@gmail.com>
2026-01-15 11:33:33 +0800

    renew parameters

--- deming.py
+++ deming.py
@@ -3,6 +3,12 @@ import pandas as pd
 from scipy.stats import t
 
 
+# 剔除偏差值（Outliers，Modified Z-Score
+def get_outliers_modified_z(s, threshold=3.5):
+    median = s.median()
+    mad = (s - median).abs().median()
+    mod_z = 0.6745 * (s - median) / mad
+    return mod_z.abs()
 
 def deming_regression(x, y, lambda_ratio=1):
     x_mean = np.mean(x)
@@ -23,10 +29,17 @@ df = pd.read_excel("update_data.xlsx", sheet_name=0)
 df['編號'] = df['編號'].astype('str')
 df['顆數'] = df['顆數'].astype('int')
 
-drugs = df['編號'].unique()
+# 剔除偏差值
+df['單重'] = df['秤重'] / df['顆數']
+df['mod_z'] = get_outliers_modified_z(df['單重'])
+# 判別準則：當 \(M_{i}>3.5\) 時，視為偏差值。依NIST (美國國家標準技術研究所) 的規範
+df['Note'] = df['mod_z'] <= 4.5 
+df_ = df.query('Note == True')
+
+drugs = df_['編號'].unique()
 parameters = []
 for i in drugs:
-    sample = df.query('編號 == @i')
+    sample = df_.query('編號 == @i')
     # 經典 Deming：lambda=1
     b0_dem_1, b1_dem_1 = deming_regression(sample['顆數'], sample['秤重'], lambda_ratio=1.0)
     parameters.append([i, float(b0_dem_1), float(b1_dem_1)])
